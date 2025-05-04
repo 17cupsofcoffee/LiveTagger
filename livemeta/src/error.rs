af256870bbafe180c000c6a4f8c6f963c8cce485
Joe Clay <27cupsofcoffee@gmail.com>
2025-05-04 15:03:15 +0100

    Use thiserror for error type
    Dependencies are already using it, so we might as well.

--- livemeta/src/error.rs
+++ livemeta/src/error.rs
@@ -1,44 +1,16 @@
-use std::fmt::{self, Display};
-
+use thiserror::Error;
 use xmp_toolkit::XmpError;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum Error {
-    Io(std::io::Error),
-    Xmp(XmpError),
+    #[error("io error")]
+    Io(#[from] std::io::Error),
+
+    #[error("xmp error")]
+    Xmp(#[from] XmpError),
+
+    #[error("missing field: {0}")]
     MissingField(&'static str),
-}
-
-impl std::error::Error for Error {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match self {
-            Error::Io(e) => Some(e),
-            Error::Xmp(e) => Some(e),
-            Error::MissingField(_) => None,
-        }
-    }
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Error::Io(_) => write!(f, "io error"),
-            Error::Xmp(_) => write!(f, "xmp error"),
-            Error::MissingField(field) => write!(f, "missing field: {}", field),
-        }
-    }
-}
-
-impl From<std::io::Error> for Error {
-    fn from(value: std::io::Error) -> Self {
-        Error::Io(value)
-    }
-}
-
-impl From<XmpError> for Error {
-    fn from(value: XmpError) -> Self {
-        Error::Xmp(value)
-    }
 }
 
 pub type Result<T = ()> = std::result::Result<T, Error>;
